584bec60a7cb708a38ffd3092f4d42351ba1423d
Sayed Murtaza <mortazasadat37@gmail.com>
2024-07-01 22:45:44 +0430

    hero

---
+++ src/app/hero-sections/page.jsx
@@ -0,0 +1,12 @@
+import HeroSimpleCenter from "@/components/hero/hero-simple-section";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      <HeroSimpleCenter />
+    </div>
+  );
+};
+
+export default Page;
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { siteConfig } from "./utils/site";
+import { siteConfig } from "../utils/site";
 
 const inter = Inter({ subsets: ["latin"] });
 
